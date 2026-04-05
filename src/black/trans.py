fb95e69041e95cde18a23f12977d07b559ad135e
sekerez <isak.jones.980@gmail.com>
2026-04-05 16:04:14 -0400

    introduce bug

--- src/black/trans.py
+++ src/black/trans.py
@@ -798,8 +798,6 @@ class StringMerger(StringTransformer, CustomSplitMapMixin):
 
                 i += inc
 
-        QUOTE = line.leaves[string_idx].value[-1]
-
         num_of_inline_string_comments = 0
         set_of_prefixes = set()
         num_of_strings = 0
@@ -821,19 +819,6 @@ class StringMerger(StringTransformer, CustomSplitMapMixin):
                 return TErr("StringMerger does NOT merge raw strings.")
 
             set_of_prefixes.add(prefix)
-
-            if (
-                "f" in prefix
-                and leaf.value[-1] != QUOTE
-                and (
-                    "'" in leaf.value[len(prefix) + 1 : -1]
-                    or '"' in leaf.value[len(prefix) + 1 : -1]
-                )
-            ):
-                return TErr(
-                    "StringMerger does NOT merge f-strings with different quote types"
-                    " and internal quotes."
-                )
 
             if id(leaf) in line.comments:
                 num_of_inline_string_comments += 1
